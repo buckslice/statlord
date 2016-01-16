20840f4275aede77bd491cb66c077f938841c1f0
buckslice <buckslice@gmail.com>
2016-01-15 22:50:13 -0800

    Fixing camera turning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,34 +7,40 @@ public class Player : MonoBehaviour {
     public bool grounded = true;
     //private float timeSinceJump = 0.0f;
     private PlayerStats stats;
+    private Transform cam;
 
     // Use this for initialization
     void Start() {
         tform = transform;
         myRigidbody = GetComponent<Rigidbody>();
         stats = GetComponent<PlayerStats>();
+        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update() {
-        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 rotdir = Input.mousePosition - pos;
-        float angle = -Mathf.Atan2(rotdir.y, rotdir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up); 
+        RaycastHit info;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out info)){
+            Vector3 fw = info.point - tform.position;
+            fw.y = 0.0f;
+            fw.Normalize();
+            transform.rotation = Quaternion.LookRotation(fw);
+        }
 
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
 
         Vector3 dir = Vector3.zero;
         if (inputX != 0.0f || inputY != 0.0f) {
-            dir = new Vector3(inputX, 0, inputY).normalized;
+            dir = cam.TransformDirection(new Vector3(inputX, 0.0f, inputY));
+            dir.y = 0.0f;
+            dir.Normalize();
         }
         dir *= stats.get(Stats.moveSpeed).value;
 
         float newY = myRigidbody.velocity.y;
 
         if (Input.GetKeyDown(KeyCode.Space) && grounded) {
-            //Debug.Log("jumped");
             newY = stats.get(Stats.jumpSpeed).value;
             grounded = false;
         }
