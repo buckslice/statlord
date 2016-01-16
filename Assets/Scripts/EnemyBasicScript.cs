bc51cf03672d4c2c80178f6fe007762b550614ad
Victor Hsiao <vwhsiao@uci.edu>
2016-01-16 13:12:13 -0800

    Enemy Basic Logistics
    Setting enemy health, spawn points, and stuff

---
+++ Assets/Scripts/EnemyBasicScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class EnemyBasicScript : MonoBehaviour {
+    public int hp;
+    private GameObject enemySpawner;
+	// Use this for initialization
+	void Awake () {
+        hp = 5;
+        enemySpawner = GameObject.Find("Spawner");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	    if (hp<=0)
+        {
+            enemySpawner.GetComponent<SpawnEnemies>().addToEnemyPool(gameObject);
+            gameObject.SetActive(false);
+        }
+	}
+
+    void OnColissionEnter(Collision collider)
+    {
+        //collider.gameObject.GetComponent<
+    }
+
+    public void SetHP(int playerLevel)
+        //set the enemy health to be an amount of the playerlevel. this one is a direct player level * 3
+    {
+        hp = playerLevel * 3;
+    }
+
+    public void SetHP(int playerLevel, int number)
+    //set the enemy health to be "number" * playerLevel
+    {
+        hp = playerLevel * number;
+    }
+
+    public void SetHP(int playerLevel, float number)
+    //set the enemy health to be "number" * playerLevel. 
+    //This allows for floating numbers to be multiplied. 
+    //aka player level * 1.5
+    {
+        hp = Mathf.RoundToInt(playerLevel * number);
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnEnemies : MonoBehaviour {
+    public GameObject enemy;
+    private GameObject player;
+    public float startTime, spawnRate;
+    public int numOfEnemies;
+    private int playerLevel;
+    private List<GameObject> enabledEnemies, disabledEnemies;
+    private GameObject[] spawnLocations;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerLevel = player.GetComponent<PlayerStats>().level;
+        spawnLocations = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        disabledEnemies = new List<GameObject>();
+        enabledEnemies = new List<GameObject>();
+    }
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i <= numOfEnemies;i++ )
+        {
+            SpawnEnemy();
+        }
+        InvokeRepeating("EnableEnemy", startTime, spawnRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void SpawnEnemy()
+    {
+        int rng = Random.Range(0, spawnLocations.Length);
+        GameObject x = Instantiate(enemy, spawnLocations[rng].transform.position, transform.rotation) as GameObject;
+        
+        x.SetActive(false);
+        disabledEnemies.Add(x);
+    }
+    void EnableEnemy()
+    {
+        try
+        {
+            disabledEnemies[0].SetActive(true);
+            disabledEnemies[0].GetComponent<EnemyBasicScript>().SetHP(playerLevel);
+            //enabledEnemies.Add(disabledEnemies[0]);
+            disabledEnemies.Remove(disabledEnemies[0]);
+            
+        }
+        catch
+        {
+            CancelInvoke();
+        }
+    }
+
+    public void addToEnemyPool(GameObject deadEnemy)
+    {
+        disabledEnemies.Add(deadEnemy);
+    }
+}
