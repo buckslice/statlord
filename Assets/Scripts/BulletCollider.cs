feb0ac8bd91d1f970511ad89bcc2742272d13348
Hao Quach <hgquach@uci.edu>
2016-01-16 12:21:07 -0800

    Commit #3 Ranged Enemy

---
+++ Assets/Scripts/BulletCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletCollider : MonoBehaviour {
+
+    public GameObject bulletModel; 
+
+    void Start()
+    {
+        Destroy(bulletModel, 3);
+    }
+    void OnCollisionEnter()
+
+    {
+        Destroy(bulletModel);
+    }
+    
+	
+}
