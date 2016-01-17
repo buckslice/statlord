39057ed64511323f723d90183a6e96dcc9ab108e
Hao Quach <hgquach@uci.edu>
2016-01-17 14:26:09 -0800

    Commit #6 Menu Beta
    Basic menu

---
+++ Assets/Scripts/TextControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TextControl : MonoBehaviour {
+
+
+void OnMouseEnter()
+    {
+
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+    }
+
+void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+void OnMouseDown()
+    {
+        SceneManager.LoadScene(1);
+        
+    }
+
+}
