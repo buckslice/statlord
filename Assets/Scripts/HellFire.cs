2f4558075769e0ebf412858550c2e8b9e47fe490
Hao Quach <hgquach@uci.edu>
2016-01-17 18:39:22 -0800

    Small Menu Changes

---
+++ Assets/Scripts/HellFire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HellFire : MonoBehaviour {
+    public GameObject fireBall;
+    public GameObject spawner;
+    public float nextFire = 0.0f;
+    public float fireRate = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(fireBall,spawner.transform.position , Quaternion.identity);
+        }
+
+    }
+}
---
+++ Assets/Scripts/HellFireProjectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HellFireProjectile : MonoBehaviour {
+    public GameObject fireBall;
+	// Use this for initialization
+	void Start () {
+       
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Destroy(fireBall, 5);
+
+	
+	}
+}
